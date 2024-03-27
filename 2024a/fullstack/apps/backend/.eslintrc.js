e45c0de742db0066cef2752f37838d31e4d0950b
Thomas Shaw <printer.83mph@gmail.com>
2024-03-27 19:03:37 -0400

    Move to other repo LOL

--- 2024a/fullstack/apps/backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ['@cis-1962/eslint-config/typescript'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-  },
-};
