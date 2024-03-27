1c4ad50c27ca2652cc7a64f81b3687cb469f2df5
Thomas Shaw <printer.83mph@gmail.com>
2024-03-27 18:49:47 -0400

    Update

--- 2024a/fullstack/apps/backend/src/main.ts
+++ 2024a/fullstack/apps/backend/src/main.ts
@@ -6,10 +6,25 @@ dotenv.config();
 const PORT = process.env.PORT ?? 8000;
 
 const app = express();
+app.use(express.json());
+
+const db: Record<string, string> = {
+  hello: 'world',
+};
 
 // define root route
-app.get('/api/hello', (_, res) => {
-  res.json({ message: 'Hello, frontend!' });
+app.get('/api/stuff', (_, res) => {
+  res.json({ things: db });
+});
+
+app.post('/api/stuff', (req, res) => {
+  if (typeof req.body !== 'object')
+    return res.status(400).send('Invalid body!');
+
+  const { key, value } = req.body;
+  db[key] = value;
+
+  return res.send('success!');
 });
 
 // listen
